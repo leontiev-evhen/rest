fbe9ed26cc6e60d7ae3df58116efebc653dd464f
Sam Smith <user11@tc.geeksforless.net>
2017-10-05 18:45:13 +0300

    valid

--- server/api/shop/libs/RestServer.php
+++ server/api/shop/libs/RestServer.php
@@ -3,57 +3,65 @@
 class RestServer
 {
 
-	public static function run ()
-	{
-		$HTTPMethod = $_SERVER['REQUEST_METHOD'];
-		$url = $_SERVER['REQUEST_URI'];
-		list($s, $a, $d, $db, $class, $params) = array_pad(explode('/', $url, 6), 6, null);
-												
-		
+    public static function run ()
+    {
+       /* foreach ($post as $key=>$item) 
+        {
+            if(isset($rules[$key]) && gettype($item) == $rules[$key]) {
+                echo 1;
+            } else {
+                echo 0;
+            }
+        }*/
+        $HTTPMethod = $_SERVER['REQUEST_METHOD'];
+        $url = $_SERVER['REQUEST_URI'];
+        list($t, $s, $a, $d, $db, $class, $params) = array_pad(explode('/', $url, 7), 7, null);
+
+
         $className = 'controllers\\'.ucfirst($class).'Controller';
- 
-		if (class_exists($className))
-		{
-			$controller = new $className;
+
+        if (class_exists($className))
+        {
+            $controller = new $className;
             $type = (preg_match('#(\.[a-z]+)#', $url, $match)) ? $match[0] : TYPE;
             $params = trim($params, '/'.$type);
-           
-			switch($HTTPMethod)
-			{
-            	case 'GET':
-                    if (!empty($params))
-                    {
-                        $method = 'AutoById';
-                    }
-                    else
-                    {
-                        $method = 'Auto';
-                    }
-			        self::setMethod($controller, 'get'.$method, $type, $params);
-			        break;
-			    case 'DELETE':
-			        self::setMethod('delete'.ucfirst($class), explode('/', $params));
-			        break;
-			    case 'POST':
-			        self::setMethod($controller, 'post'.ucfirst($class), explode('/', $params));
-			        break;
-			    case 'PUT':
-			        self::setMethod($controller, 'put'.ucfirst($class), explode('/', $params));
-			        break;
-			    default:
-			        return false;
-			}
-		}
-		else
-		{
-			header('HTTP/1.0 400 Forbidden');
-			throw new Exception ('Bad Request');
-		}
-	}
 
-	private function setMethod($class, $method, $type, $param = false)
+            switch($HTTPMethod)
+            {
+            case 'GET':
+                if (!empty($params))
+                {
+                    $method = 'AutoById';
+                }
+                else
+                {
+                    $method = 'Auto';
+                }
+                self::setMethod($controller, 'get'.$method, $type, $params);
+                break;
+            case 'DELETE':
+                self::setMethod('delete'.ucfirst($class), explode('/', $params));
+                break;
+            case 'POST':
+                self::setMethod($controller, 'post'.ucfirst($class), explode('/', $params));
+                break;
+            case 'PUT':
+                self::setMethod($controller, 'put'.ucfirst($class), explode('/', $params));
+                break;
+            default:
+                return false;
+            }
+        }
+        else
+        {
+            header('HTTP/1.0 400 Forbidden');
+            throw new Exception ('Bad Request');
+        }
+    }
+
+    private function setMethod($class, $method, $type, $param = false)
     {
-	    if ( method_exists($class, $method) )
+        if ( method_exists($class, $method) )
         {
             if ($param)
             {
@@ -63,7 +71,7 @@ class RestServer
             {
                 $data = call_user_func([$class, $method]);
             }
-	        \helpers\ConverterHelper::chooseTypeOutput($data, $type);
-	    }
-	}
+            \helpers\ConverterHelper::chooseTypeOutput($data, $type);
+        }
+    }
 }
